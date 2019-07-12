9dcb3c4d70f0f7fb15f61bbcfb7edcf763052666
amitnyc83 <amit_nyc@live.com>
2019-07-12 13:24:35 -0400

    Connect React App to Rails api with /api/shoes call

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,36 +2,28 @@ import React, { Component } from 'react';
 import './App.css'
 import Shoes from './Shoes'
 
-const shoes = [
-  {
-    name: "Tiempo",
-    brand: "Nike",
-    price: 89.99,
-    img_url: "https://www.imagehandler.net/?iset=0108&img=A1024621000&fmt=jpg&w=300&h=300&iindex=0088&c=999&cmp=100",
-    description: "A Hyperstability outsole guarantees you some of the best traction possible."
-  },
-  {
-    name: "Mercury",
-    brand: "Nike",
-    price: 109.99,
-    img_url: "https://www.imagehandler.net/?iset=0108&img=A1020562000&fmt=jpg&w=300&h=300&iindex=0088&c=999&cmp=100",
-    description: "Soft synthetic leather upper. Partial lace cover. Firm ground outsole"
-  },
-  {
-    name: "Nemeziz",
-    brand: "Adidas",
-    price: 179.99,
-    img_url: "https://www.imagehandler.net/?iset=0108&img=A1026271000&fmt=jpg&w=300&h=300&iindex=0088&c=999&cmp=100",
-    description: "The adidas Nemeziz line has always focused on agility. Let your game do the talking."
-  }
-]
+const API_URL = process.env.REACT_APP_API_URL;
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      shoes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/shoes`)
+    .then(response => response.json())
+    .then(shoes => this.setState({ shoes }))
+  }
+
+
   render() {
     return (
       <div className="App">
-      <Shoes shoes={shoes}/>
+      <Shoes shoes={this.state.shoes}/>
     </div>
   );
   }
--- src/containers/Shoes.js
+++ src/containers/Shoes.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Shoes.css';
 
 const Shoes = (props) => (
   <div className="ShoeContainer">
    <h1>Shoes</h1>
    {props.shoes.map(shoe =>
-      <div className="ShoeCard">
+      <div key={shoe.id} className="ShoeCard">
        <img className="ShoeImage" src={shoe.img_url} alt={shoe.name}/>
        <h3>{shoe.name}</h3>
        <h4>{shoe.brand}</h4>

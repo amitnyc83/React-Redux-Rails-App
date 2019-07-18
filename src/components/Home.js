352e741f53f551f8879f99f1d841101d3ae59242
amitnyc83 <amit_nyc@live.com>
2019-07-18 14:23:05 -0400

    create a homepage for app

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+  <div className="Home">
+   <h2>Welcome To Soccer Shop!</h2>
+   <p>Finally a place where you can view all the soccer shoes from around the world.</p>
+   <p>You can read about the shoes or add a new shoe to the amazing collection.</p>
+   <img src="https://i.ebayimg.com/images/i/131871381527-0-1/s-l1000.jpg" alt="Soccer Shoes" />
+  </div>
+)
+}
+
+
+export default Home;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css'
-import Shoes from './Shoes'
-import ShoeForm from './ShoeForm'
 import About from '../components/About'
+import Home from '../components/Home'
+import ShoesPage from '../components/ShoesPage'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import { Divider } from 'semantic-ui-react'
 
 
@@ -24,22 +25,21 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <React.Fragment>
-          <div className="App">
-            <NavBar />
-            <header>
-              <h1 className="app-title">Soccer Shop</h1>
-            </header>
-            <Divider hidden></Divider>
-            <Route exact path="/" component={Shoes}/>
-            <Route exact path="/shoes" component={ShoeForm}/>
+        <div className="App">
+          <NavBar />
+          <header>
+            <h1 className="app-title">Soccer Shop</h1>
+          </header>
+          <Divider hidden></ Divider>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/shoes" component={ShoesPage}/>
             <Route exact path="/about" component={About}/>
+            <Footer />
           </div>
-        </React.Fragment>
-      </Router>
-    );
+        </Router>
+      );
+    }
   }
-}
 
 
 

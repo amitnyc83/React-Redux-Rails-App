217e35eba1d4907f098d9436c2c85cde606d2f57
amitnyc83 <amit_nyc@live.com>
2019-07-18 14:24:12 -0400

    create a Footer component & add img for Homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className="row">
+     <div className="col-md-12">
+       <div className="footer p-3 mt-4 text-center bg-dark text-light">
+         Developed By:
+         <span className="text-warning font-weight-normal">
+           Amit Singh, Using React & Redux integrated with a Rails BackEnd
+         </span>
+       </div>
+     </div>
+   </div>
+  )
+}
+
+
+export default Footer;
